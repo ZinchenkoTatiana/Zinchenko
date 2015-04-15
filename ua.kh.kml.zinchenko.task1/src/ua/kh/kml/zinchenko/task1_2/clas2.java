e2fc28f002a58a41b99cb26fa4f3291f4757cb08
Таня <@Tatiana>
2015-04-15 10:57:09 +0300

    nhf

---
+++ ua.kh.kml.zinchenko.task1/src/ua/kh/kml/zinchenko/task1_2/clas2.java
@@ -0,0 +1,9 @@
+package ua.kh.kml.zinchenko.task1_2;
+
+public class clas2 {
+	  public static void main(String[] args)
+	  { 
+		  for(int i=args.length-1;i>=0;i--)
+		         System.out.print( args[i]+" " ); 
+	      } 
+}
